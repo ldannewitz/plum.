2911585b77ac9aefecae07902a9fd916f55e2365
Brad Lindgren <email": null>
2016-06-04 13:51:22 -0500

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,11 @@ m1 = Membership.create(member_id: rizzo.id, group_id: cubs_infield.id)
 m2 = Membership.create(member_id: kris.id, group_id: cubs_infield.id)
 m3 = Membership.create(member_id: addison.id, group_id: cubs_infield.id)
 
-d1 = DateTime.new(2016, 6, 4)
-d2 = DateTime.new(2016, 6, 6)
-p d1
-p d2
+event = Event.create!(name: "Roadtrip", start_date: DateTime.new(2016, 6, 4), end_date: DateTime.new(2016, 6, 6), settled?: false, group: cubs_infield)
 
-Event.create!(name: "Roadtrip", start_date: d1, end_date: d2, settled?: false, group: cubs_infield)
-
-x1 = Expense.create(event_id: 1, spender_id: rizzo.id, description: "gas", amount: 27.34, location: "Chicago") #, created_at: "2016-06-04 08:10:00", updated_at: "2016-06-04 08:10:00")
-x2 = Expense.create(event_id: 1, spender_id: rizzo.id, description: "dinner", amount: 58.21, location: "Des Moines") #, created_at: "2016-06-04 16:40:00", created_at: "2016-06-04 16:40:00")
-x3 = Expense.create(event_id: 1, spender_id: rizzo.id, description: "hotel", amount: 117.86, location: "Omaha") #, created_at: "2016-06-04 18:05:00", updated_at: "2016-06-04 18:05:00")
-x4 = Expense.create(event_id: 1, spender_id: kris.id, description: "gas", amount: 16.53, location: "Des Moines") #, created_at: "2016-06-05 09:30:00", updated_at: "2016-06-05 09:30:00")
-x5 = Expense.create(event_id: 1, spender_id: kris.id, description: "food", amount: 30.34, location: "Davenport") #, created_at: "2016-06-05 12:10:00", updated_at: "2016-06-05 12:10:00")
-x6 = Expense.create(event_id: 1, spender_id: addison.id, description: "snacks", amount: 5.21, location: "DeKalb") #, created_at: "2016-06-05 13:45:00", updated_at: "2016-06-05 13:45:00")
+x1 = Expense.create(event_id: event.id, spender_id: rizzo.id, description: "gas", amount: 27.34, location: "Chicago") #, created_at: "2016-06-04 08:10:00", updated_at: "2016-06-04 08:10:00")
+x2 = Expense.create(event_id: event.id, spender_id: rizzo.id, description: "dinner", amount: 58.21, location: "Des Moines") #, created_at: "2016-06-04 16:40:00", created_at: "2016-06-04 16:40:00")
+x3 = Expense.create(event_id: event.id, spender_id: rizzo.id, description: "hotel", amount: 117.86, location: "Omaha") #, created_at: "2016-06-04 18:05:00", updated_at: "2016-06-04 18:05:00")
+x4 = Expense.create(event_id: event.id, spender_id: kris.id, description: "gas", amount: 16.53, location: "Des Moines") #, created_at: "2016-06-05 09:30:00", updated_at: "2016-06-05 09:30:00")
+x5 = Expense.create(event_id: event.id, spender_id: kris.id, description: "food", amount: 30.34, location: "Davenport") #, created_at: "2016-06-05 12:10:00", updated_at: "2016-06-05 12:10:00")
+x6 = Expense.create(event_id: event.id, spender_id: addison.id, description: "snacks", amount: 5.21, location: "DeKalb") #, created_at: "2016-06-05 13:45:00", updated_at: "2016-06-05 13:45:00")

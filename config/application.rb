700c409a0138b5ea9c1ae385c4232a09ee5bb8f7
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-06-04 00:24:10 -0500

    Config for rack cors

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,13 @@ Bundler.require(*Rails.groups)
 
 module Settleup
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :put, :delete, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

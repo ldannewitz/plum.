e683ce1e35b9da296c67be72c52fd70592eb8f67
Brad Lindgren <email": null>
2016-06-04 14:57:21 -0500

    Payments

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,24 @@ class Event < ApplicationRecord
   has_many :invoices
 
   validates :name, :start_date, :end_date, :settled?, :group_id, :total, presence: true
+
+  def expired?
+    if self.expired?
+      create_invoices
+    end
+  end
+
+  def create_invoices
+    group_members = self.members
+    even_split = self.total/group_members.length.to_f
+    p even_split
+    # Create a hash that has user_id(keys), user_expense_total_difference(values)
+
+    # Iterate over hash and create Invoice objects
+  end
+
+  def find_member_total
+
+  end
+
 end

741c9dd5acbddd31aa17f7a256886226a2276d74
Brad Lindgren <email": null>
2016-06-04 16:07:22 -0500

    Create Invoice Objects

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,29 +1,56 @@
 class Event < ApplicationRecord
   belongs_to :group
   has_many :memberships, through: :group
-  has_many :members, through: :memberships
+  has_many :members, through: :memberships, class_name: 'User'
   has_many :expenses
   has_many :invoices
 
-  validates :name, :start_date, :end_date, :settled?, :group_id, :total, presence: true
+  validates :name, :start_date, :end_date, :group_id, :total, presence: true
 
   def expired?
-    if self.expired?
-      create_invoices
-    end
+    # is the event over?
+    self.end_date < Time.now ? create_invoices : false
   end
 
-  def create_invoices
+  def even_split
+    # find out how much each member would pay in an even split
+    self.total/self.members.count.to_f
+  end
+
+  def get_member_balances
+    invoice_hash = {}
     group_members = self.members
-    even_split = self.total/group_members.length.to_f
-    p even_split
-    # Create a hash that has user_id(keys), user_expense_total_difference(values)
 
-    # Iterate over hash and create Invoice objects
+    # find how much each person owes
+    group_members.each do |member|
+      member_total = find_member_total(self.expenses, member)
+      invoice_hash[member.email] = member_total - self.even_split
+    end
+    # return payment amounts in a hash
+    invoice_hash
   end
 
-  def find_member_total
+  def find_member_total(expenses, member)
+    total = 0
+    expenses.each { |expense| total += expense.amount if expense.spender_id == member.id }
+    total
+  end
 
+  def generate_invoice_objects
+    invoices = self.get_member_balances
+    invoices.each do |invoice|
+      # Get user
+      user = User.find_by(email: invoice[0])
+      # Get invoice type
+      invoice_type = nil
+      if invoice[1] > 0
+        invoice_type = 'credit'
+      elsif invoice[1] < 0
+        invoice_type = 'debit'
+      end
+      # create invoice object
+      Invoice.create!(event_id: self.id, user_id: user.id, invoice_type: invoice_type, amount: invoice[1], satisfied?: false)
+    end
   end
 
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,5 @@
 class Membership < ApplicationRecord
-  belongs_to :member
+  belongs_to :member, class_name: 'User'
   belongs_to :group
 
   validates :member_id, :group_id, presence: true

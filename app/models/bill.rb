a269d6b9ab306b8f1c760719cfe6256cfd418b73
bplindgren <bplindgren91@gmail.com>
2016-06-06 14:45:22 -0500

    Payouts

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -16,31 +16,27 @@ class Bill < ApplicationRecord
           :password  => ENV['PASSWORD'],
           :signature => ENV['SIGNATURE'])
 
-    p "======="
-    p self
-    p "======="
-
     # Build request object
     @get_invoice_details = @api.build_get_invoice_details({
-      :invoiceID => self.paypal_invoice_id }) #"INV2-88A8-UXGK-LRNR-WEA9" })
+      :invoiceID => self.paypal_id }) #"INV2-88A8-UXGK-LRNR-WEA9" })
 
     # Make API call & get response
     @get_invoice_details_response = @api.get_invoice_details(@get_invoice_details)
 
     # Access Response
-    # if @get_invoice_details_response.success?
-    if @get_invoice_details_response.paymentDetails
-      p "good"
-      p self.update_attribute(:satisfied?, true)
-      self.save
+    if @get_invoice_details_response.success?
       # p @get_invoice_details_response
-      # @get_invoice_details_response.invoice
-      # @get_invoice_details_response.invoiceDetails
-      # @get_invoice_details_response.refundDetails
-      # @get_invoice_details_response.invoiceURL
-      p self
+      p @get_invoice_details_response.paymentDetails.paypalPayment.amount
+      if @get_invoice_details_response.paymentDetails.paypalPayment.amount == self.amount * (-1)
+        self.update_attribute(:satisfied?, true)
+        self.save
+        # p @get_invoice_details_response
+        # @get_invoice_details_response.invoice
+        # @get_invoice_details_response.invoiceDetails
+        # @get_invoice_details_response.refundDetails
+        # @get_invoice_details_response.invoiceURL
+      end
     else
-      p "bad"
       @get_invoice_details_response.error
     end
   end

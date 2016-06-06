5342eeda15a192a403cd1eef3dcff3998e7712f3
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-06-06 10:00:08 -0500

    Refactor spec

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -6,58 +6,78 @@ RSpec.describe GroupsController, :type => :controller do
     let(:json) { NEW_GROUP_JSON }
 
     it "responds successfully with an HTTP 204 status code" do
-      post :create, params: json
-      expect(response).to be_success
-      expect(response).to have_http_status(204)
+      silence_stream(STDOUT) do
+        post :create, params: json
+        aggregate_failures "testing response" do
+          expect(response).to be_success
+          expect(response).to have_http_status(204)
+        end
+      end
     end
 
     it "creates a new group" do
-      expect {post :create, params: json}.to change {Group.all.count}.by(1)
+      silence_stream(STDOUT) do
+        expect {post :create, params: json}.to change { Group.all.count }.by(1)
+      end
     end
   end
 
   describe "GET #show" do
-    let(:group) {Group.create!(name: "Cubs")}
+    let(:group) { Group.create!(name: "Cubs") }
 
     it "responds successfully with an HTTP 200 status code" do
-      get :show, params: { id: group.id }
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
+      silence_stream(STDOUT) do
+        get :show, params: { id: group.id }
+        aggregate_failures "testing response" do
+          expect(response).to be_success
+          expect(response).to have_http_status(200)
+        end
+      end
     end
 
     it "loads a group into @group" do
-      get :show, params: { id: group.id }
-
-      expect(assigns(:group)).to match(group)
+      silence_stream(STDOUT) do
+        get :show, params: { id: group.id }
+        expect(assigns(:group)).to match(group)
+      end
     end
   end
 
   describe "GET #members" do
-    let(:group) {Group.create!(name: "Cubs")}
+    let(:group) { Group.create!(name: "Cubs") }
 
     it "responds successfully with an HTTP 200 status code" do
-      get :members, params: { id: group.id }
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
+      silence_stream(STDOUT) do
+        get :members, params: { id: group.id }
+        aggregate_failures "testing response" do
+          expect(response).to be_success
+          expect(response).to have_http_status(200)
+        end
+      end
     end
 
     it "loads a group's members into @members" do
-      members = group.members
-      get :members, params: { id: group.id }
-
-      expect(assigns(:members)).to match_array(members)
+      silence_stream(STDOUT) do
+        members = group.members
+        get :members, params: { id: group.id }
+        expect(assigns(:members)).to match_array(members)
+      end
     end
   end
 
   describe "POST #add_members" do
-    let(:group) {Group.create!(name: "Cubs")}
+    let(:group) { Group.create!(name: "Cubs") }
     let(:json) { NEW_MEMBER_JSON }
 
     it "responds successfully with an HTTP 204 status code" do
-      json["id"] = group.id
-      post :add_members, params: json
-      expect(response).to be_success
-      expect(response).to have_http_status(204)
+      silence_stream(STDOUT) do
+        json["id"] = group.id
+        post :add_members, params: json
+        aggregate_failures "testing response" do
+          expect(response).to be_success
+          expect(response).to have_http_status(204)
+        end
+      end
     end
 
     # it "adds members to a group" do
@@ -66,19 +86,24 @@ RSpec.describe GroupsController, :type => :controller do
   end
 
   describe "GET #events" do
-    let(:group) {Group.create!(name: "Cubs")}
+    let(:group) { Group.create!(name: "Cubs") }
 
     it "responds successfully with an HTTP 200 status code" do
-      get :events, params: { id: group.id }
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
+      silence_stream(STDOUT) do
+        get :events, params: { id: group.id }
+        aggregate_failures "testing response" do
+          expect(response).to be_success
+          expect(response).to have_http_status(200)
+        end
+      end
     end
 
     it "loads a group's events into @events" do
-      events = group.events
-      get :events, params: { id: group.id }
-
-      expect(assigns(:events)).to match_array(events)
+      silence_stream(STDOUT) do
+        events = group.events
+        get :events, params: { id: group.id }
+        expect(assigns(:events)).to match_array(events)
+      end
     end
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,11 +2,17 @@ require_relative '../rails_helper'
 
 RSpec.describe Event, type: :model do
 
-  let!(:member) { User.create!(first_name: 'First', last_name: 'Last', email: 'e@mail.com', password: 'password') }
-  let (:cubs_infield) { Group.create!(name: "Cubs") }
-  let!(:membership) { Membership.create!(member: member, group: cubs_infield) }
+  before(:each) do
+    @member = User.create!(first_name: 'First', last_name: 'Last', email: 'e@mail.com', password: 'password')
+    @cubs_infield = Group.create!(name: "Cubs")
+    @membership = Membership.create!(member: @member, group: @cubs_infield)
+    @event = Event.create!(name: "Roadtrip", start_date: DateTime.new(2016, 6, 4), end_date: DateTime.new(2016, 6, 20), settled?: false, group: @cubs_infield, total: 10.00)
+  end
 
-  let(:event) { Event.create!(name: "Roadtrip", start_date: DateTime.new(2016, 6, 4), end_date: DateTime.new(2016, 6, 20), settled?: false, group: cubs_infield, total: 10.00) }
+  # let!(:member) { User.create!(first_name: 'First', last_name: 'Last', email: 'e@mail.com', password: 'password') }
+  # let (:cubs_infield) { Group.create!(name: "Cubs") }
+  # let!(:membership) { Membership.create!(member: member, group: cubs_infield) }
+  # let(:event) { Event.create!(name: "Roadtrip", start_date: DateTime.new(2016, 6, 4), end_date: DateTime.new(2016, 6, 20), settled?: false, group: cubs_infield, total: 10.00) }
 
   it 'has a name' do
     should { validate_presence_of(:name) }
@@ -54,51 +60,51 @@ RSpec.describe Event, type: :model do
 
   describe '#expired?' do
     it 'returns false when the event is live' do
-      expect(event.expired?).to be(false)
+      expect(@event.expired?).to be(false)
     end
 
     it 'calls #generate_invoices when the event is expired' do
-      event.end_date = DateTime.new(2016, 6, 5)
+      @event.end_date = DateTime.new(2016, 6, 5)
       # puts event.end_date
       # puts DateTime.now
       # puts event.end_date < DateTime.now
-      event.should_receive(:generate_invoices)
-      event.expired?
+      @event.should_receive(:generate_invoices)
+      @event.expired?
     end
   end
 
   describe '#even_split' do
     it 'splits the event total between the group members' do
-      expect(event.even_split).to eq(10)
+      expect(@event.even_split).to eq(10)
     end
   end
 
   describe '#get_member_balances' do
     it 'determines the balance of each group members' do
-      expect(event.get_member_balances).to include("e@mail.com"=>-10.0)
+      expect(@event.get_member_balances).to include("e@mail.com"=>-10.0)
     end
   end
 
   describe '#generate_invoices' do
-    let!(:expense) { Expense.create!(event: event, spender_id: member.id, description: "Redbull", amount: 6.20) }
+    let!(:expense) { Expense.create!(event: @event, spender_id: @member.id, description: "Redbull", amount: 6.20) }
     let!(:friend) { User.create!(first_name: 'Friend', last_name: 'Person', email: 'em@ail.com', password: 'password') }
-    let!(:membershippp) { Membership.create!(member: friend, group: cubs_infield) }
+    let!(:membershippp) { Membership.create!(member: friend, group: @cubs_infield) }
 
     it 'generates a debit bill for a user who owes money' do
-      member.expenses.first.update(amount: 0)
-      expect{event.generate_invoices}.to change{Bill.all.count}.by(2)
-      expect(member.bills.first.bill_type).to eq('debit')
+      @member.expenses.first.update(amount: 0)
+      expect{@event.generate_invoices}.to change{Bill.all.count}.by(2)
+      expect(@member.bills.first.bill_type).to eq('debit')
     end
 
     it 'generates a credit bill for a user who is owed money' do
-      event.update(total: 10)
-      expect{event.generate_invoices}.to change{member.bills.count}.by(1)
-      expect(member.bills.first.bill_type).to eq('credit')
+      @event.update(total: 10)
+      expect{@event.generate_invoices}.to change{@member.bills.count}.by(1)
+      expect(@member.bills.first.bill_type).to eq('credit')
     end
 
     it 'does not generate an invoice if a user has no balance' do
-      event.update(total: 12.4)
-      expect{event.generate_invoices}.not_to change{member.bills.count}
+      @event.update(total: 12.4)
+      expect{@event.generate_invoices}.not_to change{@member.bills.count}
     end
   end
 

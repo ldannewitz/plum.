39f9a4b48941fd2c43ded3c71477ad0633e8be55
bplindgren <bplindgren91@gmail.com>
2016-06-08 15:12:45 -0500

    FIXES FOR REAL THIS TIME

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 require 'securerandom'
 class User < ApplicationRecord
-  before_create :set_auth_token
   has_secure_password
 
   has_many :memberships, foreign_key: :member_id, dependent: :destroy
@@ -28,17 +27,6 @@ class User < ApplicationRecord
   #     total += find_member_total(event.expenses, self) - event.even_split
   #   end
   #   total
-  # end
-
-  # private
-
-  # def set_auth_token
-  #   return if auth_token.present?
-  #   self.auth_token = generate_auth_token
-  # end
-
-  # def generate_auth_token
-  #   SecureRandom.hex(15)
   # end
 
 end

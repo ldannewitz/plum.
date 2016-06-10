17e4a17984e0db44a2020ddb58e9b7894def95af
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-06-09 22:43:16 -0500

    Sessions controller spec

--- spec/support/json_testers.rb
+++ spec/support/json_testers.rb
@@ -1,7 +1,9 @@
-NEW_EXPENSE_JSON = {"description"=>"Gas", "location"=>"Des Moines", "amount"=>"50", "event_id"=>"4", "spender_id"=>"1", "id"=>"1", "event"=>{}}
+NEW_EXPENSE_JSON = { "description"=>"Gas", "location"=>"Des Moines", "amount"=>"50", "event_id"=>"4", "spender_id"=>"1", "id"=>"1", "event"=>{} }
 
-NEW_GROUP_JSON = {"name"=>"Params group", "members"=>[{"email"=>"Lisa@gmail.com"}, {"email"=>"Brad@gmail.com"}, {"email"=>"Tom@gmail.com"}, {"email"=>"Arizzo@gmail.com"}, {"email"=>"Krisb6579@gmail.com"}], "user_id"=>"7", "group"=>{"name"=>"Params group"}}
+NEW_GROUP_JSON = { "name"=>"Params group", "members"=>[{"email"=>"Lisa@gmail.com"}, {"email"=>"Brad@gmail.com"}, {"email"=>"Tom@gmail.com"}, {"email"=>"Arizzo@gmail.com"}, {"email"=>"Krisb6579@gmail.com"}], "user_id"=>"7", "group"=>{"name"=>"Params group"} }
 
-NEW_EVENT_JSON = {"name" => "Test Party", "start_date" => DateTime.new(2016, 6, 4), "end_date" => DateTime.new(2016, 6, 7), "group" => "Cubs"}
+LOGIN_JSON = { "email"=>"arizzo@gmail.com", "password"=>"password" }
 
-NEW_USER_JSON = {"first_name" => "brad","last_name" => "L","email" => "e@maillllllll.com","password" => "secret"}
+NEW_EVENT_JSON = { "name" => "Test Party", "start_date" => DateTime.new(2016, 6, 4), "end_date" => DateTime.new(2016, 6, 7), "group" => "Cubs" }
+
+NEW_USER_JSON = { "first_name" => "brad","last_name" => "L","email" => "e@maillllllll.com","password" => "secret" }

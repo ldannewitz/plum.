505fc0300e0106ae80f9a2cf764fab1aa9cc1d47
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-06-22 19:01:36 -0500

    Attempt test for event_controller_spec POST #new_expense

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
     if @expense.save
       render json: @expense, status: :created, location: @expense
     else
+      p @expense.errors
       render json: @expense.errors, status: :unprocessable_entity
     end
   end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,14 +2,15 @@ require "rails_helper"
 
 RSpec.describe EventsController, :type => :controller do
 
+  before(:each) do
+    @member = User.create!(first_name: 'First', last_name: 'Last', email: 'e@mail.com', password: 'password')
+    @rizzo = User.create!(first_name: "Anthony", last_name: "Rizzo", email: "arizzo@gmail.com", password: "password")
+    @cubs_infield = Group.new(name: "Cubs", members: [@member, @rizzo])
+    @event = Event.create!(name: "Roadtrip", start_date: DateTime.new(2016, 6, 4), end_date: DateTime.new(2016, 6, 6), settled?: false, group: @cubs_infield)
+  end
+
   describe "GET #show" do
 
-    before(:each) do
-      @member = User.create!(first_name: 'First', last_name: 'Last', email: 'e@mail.com', password: 'password')
-      @rizzo = User.create!(first_name: "Anthony", last_name: "Rizzo", email: "arizzo@gmail.com", password: "password")
-      @cubs_infield = Group.new(name: "Cubs", members: [@member, @rizzo])
-      @event = Event.create!(name: "Roadtrip", start_date: DateTime.new(2016, 6, 4), end_date: DateTime.new(2016, 6, 6), settled?: false, group: @cubs_infield)
-    end
 
     it "responds successfully with an HTTP 200 status code" do
       silence_stream(STDOUT) do
@@ -33,15 +34,19 @@ RSpec.describe EventsController, :type => :controller do
 
     # it "responds successfully with an HTTP 201 status code" do
     #   # silence_stream(STDOUT) do
+    #     NEW_EXPENSE_JSON["id"] = @event.id
+    #     NEW_EXPENSE_JSON["event_id"] = @event.id
+    #     NEW_EXPENSE_JSON["spender_id"] = @rizzo.id
     #     post :new_expense, params: NEW_EXPENSE_JSON
     #     aggregate_failures "testing response" do
-    #       assert_response :success
+    #       # assert_response :success
+    #       expect(response).to be_success
     #       expect(response).to have_http_status(201)
     #     end
     #   # end
     # end
     #
-    # it "creates a new user" do
+    # it "creates a new expense" do
     #   # silence_stream(STDOUT) do
     #     expect { post :new_expense, params: NEW_EXPENSE_JSON }.to change { Expense.all.count }.by(1)
     #   # end
@@ -49,7 +54,7 @@ RSpec.describe EventsController, :type => :controller do
 
     it 'returns an error for invalid params' do
       silence_stream(STDOUT) do
-        post :new_expense, params: {"location"=>"Des Moines", "amount"=>"50", "event_id"=>"4", "spender_id"=>"1", "id"=>"1", "event"=>{}}
+        post :new_expense, params: {"location"=>"Des Moines", "id"=>"1"}
         expect(response).to have_http_status(422)
       end
     end
